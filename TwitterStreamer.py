aa8ab2513d8bb1fa27869afbc7c4a2730c0e8115
Joel Benson <joel.benson@wustl.edu>
2019-06-10 22:06:18 -0500

    Update TwitterStreamer.py
    Now appends the tweets to a file containing the state abbreviation.

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -13,16 +13,32 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 class StreamListener(tweepy.StreamListener):
+    def writeStatusToFile(state, text):
+        fileName = state + ".txt"
+        f = open(fileName,'a+')
+        f.write(text + "\n -------------------------------- \n")
+        f.close()
+    def getState(place):
+        state = 'x'
+        tuple = place.split(', ')
+        if (len(tuple) > 1):
+            state = tuple[1]
+        if (len(state) == 2):
+            return state;
+        return "NAS"
     def on_status(self, status):
-
         if not(status.text[0] == '@'):
             print(status.text)
-            print(status.place.full_name)
+            state = StreamListener.getState(status.place.full_name)
+            StreamListener.writeStatusToFile(state, status.text)
+            print(state)
             print(" ")
     def on_error(self, status_code):
         if status_code == 420:
-            return False;
+            return False
+
 
 stream_listener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(locations=[-96.899414,43.612217,-91.933594,47.694974])
+stream.filter(locations=[-123.750000,28.304381,-67.675781,48.806863])
+
