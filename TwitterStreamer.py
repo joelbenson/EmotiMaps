5c53220db65658d0d06bf4af04d62d17f046652b
Joel Benson <joelbenson@Joels-MacBook-Pro.local>
2019-06-13 23:44:36 -0500

    Moved code to main function

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -2,17 +2,6 @@ import json
 import tweepy
 from EmotionRanker import get_emotion_of_tweet
 
-consumer_key = "TC0gBTsiLpQK43kcVWLYwysr9"
-consumer_secret = "A0GfMBG2O2IykxbaTNvqfvAUxQSmtjpNO2qaJgxd4di6mKlhWV"
-
-access_token = "1535031613-v8wW2PEksDe8VPhdRXPwB58iOdpwpojMKABZKlK"
-access_token_secret  = "v5LIWJLfXEzPUMni4HT2gatJMYviCdlx56nR9pIHmRj6N"
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
 class StreamListener(tweepy.StreamListener):
     def writeStatusToFile(state, text):
         fileName = state + ".txt"
@@ -31,21 +20,12 @@ class StreamListener(tweepy.StreamListener):
         if not(status.text[0] == '@'):
             print(status.text)
             state = StreamListener.getState(status.place.full_name)
-            try: 
+            try:
                 StreamListener.writeStatusToFile(state, status.text)
             except:
-                print("emoji tweet") 
+                print("emoji tweet")
             print(state)
             print(" ")
     def on_error(self, status_code):
         if status_code == 420:
             return False
-
-
-# stream_listener = StreamListener()
-# stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-# stream.filter(locations=[-167.695313,16.804541,-60.996094,72.019729])
-
-with open("CA.txt", "r") as f:
-    for line in f: 
-        get_emotion_of_tweet(line)
---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import tweepy
+from TwitterStreamer import StreamListener
+from EmotionRanker import get_emotion_of_tweet
+
+def main():
+    #Access to Twitter API
+    consumer_key = "TC0gBTsiLpQK43kcVWLYwysr9"
+    consumer_secret = "A0GfMBG2O2IykxbaTNvqfvAUxQSmtjpNO2qaJgxd4di6mKlhWV"
+    access_token = "1535031613-v8wW2PEksDe8VPhdRXPwB58iOdpwpojMKABZKlK"
+    access_token_secret  = "v5LIWJLfXEzPUMni4HT2gatJMYviCdlx56nR9pIHmRj6N"
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    #Opening Stream
+    stream_listener = StreamListener()
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+
+    #Choose coordinate box for tweet locations
+    stream.filter(locations=[-167.695313,16.804541,-60.996094,72.019729])
+    time.sleep(10)
+    stream.disconnect()
+    print("done")
+    #with open("CA.txt", "r") as f:
+    #    for line in f:
+    #        get_emotion_of_tweet(line)
+
+
+if __name__ == "__main__":
+    main()
