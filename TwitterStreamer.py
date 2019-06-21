6cbef3c8c6e32c2c785b844d99124c6e3c9e7efc
Joel Benson <joelbenson@Joels-MacBook-Pro.local>
2019-06-20 20:21:31 -0500

    Streamer terminates after some set value of tweets and calls emotion function to print out emotions of tweet each time.

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -1,7 +1,10 @@
 import json
 import tweepy
+from EmotionRanker import get_emotion_of_tweet
 
 class StreamListener(tweepy.StreamListener):
+    count = 0
+    MAX_TWEETS = 50
     def writeStatusToFile(state, text):
         fileName = "StateTweets/" + state + ".txt"
         f = open(fileName,'a+')
@@ -15,14 +18,27 @@ class StreamListener(tweepy.StreamListener):
         if (len(state) == 2):
             return state
         return "NAS"
+    #Executed for each tweet
     def on_status(self, status):
+        #Do not account for reply threads
         if not(status.text[0] == '@'):
+            #print to command line
             print(status.text)
+            #get state location of tweet
             state = StreamListener.getState(status.place.full_name)
+            #Write tweet to correct state file and
+            #don't break if tweet contains emoji
             try:
                 StreamListener.writeStatusToFile(state, status.text)
             except:
                 print("emoji tweet")
+            #get dictionary of emotions from the tweet
+            #dict emotions = get_emotion_of_tweet(status.text)
+            #print out the emotions of the tweet_word_list
+            #for x, y in dict.items():
+            #    print(x, y)
+            if (StreamListener.count > StreamListener.MAX_TWEETS):
+                return False
             print(state)
             print(" ")
     def on_error(self, status_code):
